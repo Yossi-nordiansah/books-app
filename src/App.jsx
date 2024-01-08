823a29d1f94ec8008513506e5a419fc0710970fa
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2024-01-08 21:51:29 +0700

    add lorem

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,6 +7,7 @@ function App() {
   return (
     <>
       <h1 className='text-center'>Hello World</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dolores doloremque, odit quis iure dolorem totam, eius distinctio, nisi asperiores quidem fugiat quas vitae? Veniam nostrum voluptatibus repellat harum cum. Suscipit distinctio, velit vero porro quaerat sunt facere tenetur modi rerum repellendus quisquam inventore dolorem eum veniam maxime soluta aliquid harum dolorum reiciendis error, nemo debitis? Accusantium aliquid, minima quod eveniet adipisci consectetur harum laudantium exercitationem? Optio vel maiores, rerum harum nulla inventore repellat dolorum id ratione quam, ab, sit cupiditate numquam. Consequatur atque provident nisi amet reprehenderit cum praesentium quam numquam corrupti nemo, optio error totam eaque, non architecto?</p>
     </>
   )
 }
