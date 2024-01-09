a505b172e66a121fc2ecb19483d1209426f4fd83
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2024-01-09 10:30:49 +0700

    add cancel link

--- src/pages/AddBookPage.jsx
+++ src/pages/AddBookPage.jsx
@@ -81,7 +81,7 @@ const AddBookPage = ({books, setBooks}) => {
         </button>
         <button
           type="button"
-          onClick={(books) => navigate('/')}
+          onClick={(books) => navigate('/books-app')}
           class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-12 py-2 mt-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
         >
           Batal
--- src/pages/EditBookPage.jsx
+++ src/pages/EditBookPage.jsx
@@ -78,7 +78,7 @@ const EditBookPage = ({ books, setBooks }) => {
         </button>
         <button
           type="button"
-          onClick={(books) => navigate('/')}
+          onClick={(books) => navigate('/books-app')}
           class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-12 py-2 mr-2 mt-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
         >
           Batal
